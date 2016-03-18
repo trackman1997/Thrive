84bb3be518292aa223d190d56dfc0ae1236c9011
sdkamer <sdkamer@gmail.com>
2016-03-17 22:49:30 -0400

    Just a few changes/additions

--- src/general/task_manager.cpp
+++ src/general/task_manager.cpp
@@ -1,11 +1,9 @@
 #include "task_manager.h"
 
-#include <boost/thread.hpp>
-
 using namespace thrive;
 
 struct TaskManager::Implementation{
-    unsigned short m_availableThreads = boost::thread::hardware_concurrency()-1;
+    size_t m_availableThreads = boost::thread::hardware_concurrency();
 };
 
 TaskManager&
@@ -21,8 +19,29 @@ TaskManager::TaskManager()
 
 TaskManager::~TaskManager() { }
 
-unsigned short
+size_t
 TaskManager::getAvailableThreads()
 {
-    return m_impl->m_availableThreads;
+    return m_availableThreads;
+}
+
+void
+TaskManager::start()
+{
+    m_running = true;
+
+    for(size_t i = 0; i < m_availableThreads; ++i)
+        m_threads.push_back(std::unique_ptr<boost::thread>(new boost::thread(boost::bind(&TaskManager::worker,this))));
+
+    while (m_running)
+        boost::this_thread::yield();
+}
+
+void
+TaskManager::worker()
+{
+    while(m_running)
+    {
+
+    }
 }
--- src/general/task_manager.h
+++ src/general/task_manager.h
@@ -3,10 +3,20 @@
 #ifndef TASKMANAGER_H
 #define TASKMANAGER_H
 
+#include <boost/thread.hpp>
 #include <memory>
+#include <vector>
 
 namespace thrive
 {
+
+class Task
+{
+    public:
+        template<class F>
+        Task();
+        ~Task();
+};
 
 class TaskManager
 {
@@ -25,12 +35,18 @@ class TaskManager
         */
         ~TaskManager();
 
-        unsigned short getAvailableThreads();
+        size_t getAvailableThreads();
 
     protected:
 
     private:
         TaskManager();
+        void start();
+        void worker();
+        bool m_running;
+
+        size_t m_availableThreads;
+        std::vector<std::unique_ptr<boost::thread>> m_threads;
 
         struct Implementation;
         std::unique_ptr<Implementation> m_impl;
