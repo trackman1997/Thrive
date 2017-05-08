25c5a89df5e8c31344b215a47c3f418f01e6d616
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2017-05-08 23:12:31 +0300

    Added helper for SVN committing to avoid committing files in Source

---
+++ SVNCommit.rb
@@ -0,0 +1,72 @@
+#!/usr/bin/env ruby
+# Helper for commiting the Content folder to svn
+# without accidentally committing anything from Source
+
+require_relative 'linux_setup/RubyCommon.rb'
+
+if ARGV.length < 1
+  
+  onError "Commit message required for committing"
+  exit 2
+  
+end
+
+
+COMMIT_MESSAGE = ARGV.join(" ").gsub(/\"/, "'")
+
+if COMMIT_MESSAGE.empty?
+
+  onError "Commit message is empty"
+  exit 2
+  
+end
+
+# Detect Source stuff about to be committed
+SOURCE_CHECK = Regexp.new(/a\s+Source/i).freeze
+
+statusStr = `svn status`.strip
+
+if SOURCE_CHECK =~ statusStr
+
+  puts statusStr
+  warning "Was about to commit Source folder contents to svn"
+
+  cmd = "svn rm --keep-local Source"
+  
+  info "Running '#{cmd}'"
+
+  systemChecked cmd
+  
+end
+
+# Don't commit if nothing there
+actualFilesStatus = `svn status`.strip
+
+commitCount = 0
+
+actualFilesStatus.each_line{|line|
+
+  if line.length > 0 && line[0] != '?'
+
+    #puts "thing to commit: #{line}"
+    commitCount += 1
+    
+  end
+}
+
+if commitCount == 0
+
+  warning "Nothing to commit"
+  exit 0
+  
+end
+
+info "committing #{commitCount} changes with message: \"#{COMMIT_MESSAGE}\""
+
+system "svn commit -m \"#{COMMIT_MESSAGE}\""
+if $?.exitstatus > 0
+  onError "Committing failed!"
+  exit 1
+end
+
+success "Committed"
