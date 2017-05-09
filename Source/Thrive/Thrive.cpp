212ba1025482471e3d4481277e75e766963e03d8
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2017-05-09 22:58:15 +0300

    Added logging functions

--- Source/Thrive/Thrive.cpp
+++ Source/Thrive/Thrive.cpp
@@ -3,3 +3,14 @@
 #include "Thrive.h"
 
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Thrive, "Thrive" );
+
+
+
+DEFINE_LOG_CATEGORY(ThriveLog);
+
+
+//DEFINE_LOG_CATEGORY(CellLog);
+ 
+//Logging for Critical Errors that must always be addressed
+DEFINE_LOG_CATEGORY(ThriveCriticalErrors);
+
--- Source/Thrive/Thrive.h
+++ Source/Thrive/Thrive.h
@@ -4,3 +4,26 @@
 
 #include "Engine.h"
 
+
+// Log gategories
+// Log colours are defined by the severities which are: Log,Warning,Error,Fatal
+
+//General Log
+DECLARE_LOG_CATEGORY_EXTERN(ThriveLog, Log, All);
+
+
+//DECLARE_LOG_CATEGORY_EXTERN(CellLog, Log, All);
+ 
+//Logging for Critical Errors that must always be addressed
+DECLARE_LOG_CATEGORY_EXTERN(ThriveCriticalErrors, Log, All);
+
+// Quick access to printing text with no extra formatting
+#define LOG_LOG(x) UE_LOG(ThriveLog, Log, TEXT(x));
+#define LOG_WARNING(x) UE_LOG(ThriveLog, Warning, TEXT(x));
+#define LOG_ERROR(x) UE_LOG(ThriveLog, Error, TEXT(x));
+#define LOG_FATAL(x) UE_LOG(ThriveCriticalErrors, Fatal, TEXT(x));
+
+// Shows up on screen
+#define DISPLAY_MESSAGE(text) if (GEngine) \
+ GEngine->AddOnScreenDebugMessage(-1, 1.5f, FColor::Blue, text);
+
