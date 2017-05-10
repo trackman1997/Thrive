ba2fc023f9c74f4886d225ec08dceaa0a4a017cb
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2017-05-10 22:17:55 +0300

    The code taken from the old membrane system now compiles

--- Source/Thrive/Microbe/MembraneComponent.cpp
+++ Source/Thrive/Microbe/MembraneComponent.cpp
@@ -3,6 +3,7 @@
 #include "Thrive.h"
 #include "MembraneComponent.h"
 
+#include <cmath>
 
 
 UMembraneComponent::UMembraneComponent() : Super(){
@@ -19,33 +20,257 @@ void UMembraneComponent::BeginPlay(){
 void UMembraneComponent::CreateMembraneMesh(URuntimeMeshComponent* GeometryReceiver){
 
     if(OrganelleContainerComponent){
+
+        // TODO: apply organelle data
         
-        
-    } else {
+    }
 
-        TArray<FVector> Vertices;
-        TArray<FVector> Normals;
-        TArray<FRuntimeMeshTangent> Tangents;
-        TArray<FVector2D> TextureCoordinates;
-        TArray<int32> Triangles;
+    // Dummy data
+    for(int x = -4; x < 5; ++x){
 
-        URuntimeMeshLibrary::CreateBoxMesh(FVector(50, 50, 50), Vertices, Triangles, Normals,
-            TextureCoordinates, Tangents);
+        for(int y = -4; y < 5; ++y){
 
-        // Create the mesh section specifying collision
-        GeometryReceiver->CreateMeshSection(0, Vertices, Triangles, Normals, TextureCoordinates,
-            TArray<FColor>(), Tangents, true, EUpdateFrequency::Infrequent);
-
-        GeometryReceiver->AddCollisionConvexMesh(Vertices);
-
-        if(GeometryReceiver->GetBodyInstance())
-        {
-            GeometryReceiver->GetBodyInstance()->bLockXRotation = true;
-            GeometryReceiver->GetBodyInstance()->bLockYRotation = true;
-
-            GeometryReceiver->GetBodyInstance()->SetDOFLock(EDOFMode::XYPlane);
+            OrganellePositions.Emplace(FVector2D(x, y));
         }
+    }
+
+    // Generate mesh data //
+
+    MeshPoints.Empty();
+
+    // This initializes the 2D points and then starts moving them towards organelles
+    DoMembraneIterativeShrink();
+
+    // One more iteration
+    DrawMembrane();
+
+    // Setup 3D points
+    MakePrism();
+    CalcUVCircle();
+
+
+    // Create renderable from the mesh data //
+
+    TArray<FVector> Vertices;
+    TArray<FVector> Normals;
+    TArray<FRuntimeMeshTangent> Tangents;
+    TArray<FVector2D> TextureCoordinates;
+    TArray<int32> Triangles;
+
+    URuntimeMeshLibrary::CreateBoxMesh(FVector(50, 50, 50), Vertices, Triangles, Normals,
+        TextureCoordinates, Tangents);
+
+    // Create the mesh section specifying collision
+    GeometryReceiver->CreateMeshSection(0, Vertices, Triangles, Normals, TextureCoordinates,
+        TArray<FColor>(), Tangents, true, EUpdateFrequency::Infrequent);
+
+    GeometryReceiver->AddCollisionConvexMesh(Vertices);
+
+    if(GeometryReceiver->GetBodyInstance())
+    {
+        GeometryReceiver->GetBodyInstance()->bLockXRotation = true;
+        GeometryReceiver->GetBodyInstance()->bLockYRotation = true;
+
+        GeometryReceiver->GetBodyInstance()->SetDOFLock(EDOFMode::XYPlane);
     }
 }
 
+// ------------------------------------ //
+void UMembraneComponent::DoMembraneIterativeShrink(){
+
+    // Reset dimensions before calculating the new required dimensions
+    CellDimensions = 10;
+
+    for (FVector2D pos : OrganellePositions) {
+        if (std::fabs(pos.X) + 1 > CellDimensions) {
+            CellDimensions = std::abs(pos.X) + 1;
+        }
+        if (std::fabs(pos.Y) + 1 > CellDimensions) {
+            CellDimensions = std::abs(pos.Y) + 1;
+        }
+    }
+
+    Vertices2D.Empty();
+
+	for(int i = 0; i < MembraneResolution; i++)
+	{
+		Vertices2D.Emplace(-CellDimensions + 2*CellDimensions/MembraneResolution*i,
+            -CellDimensions);
+	}
+	for(int i = 0; i < MembraneResolution; i++)
+	{
+		Vertices2D.Emplace(CellDimensions,
+            -CellDimensions + 2*CellDimensions/MembraneResolution*i);
+	}
+	for(int i = 0; i < MembraneResolution; i++)
+	{
+		Vertices2D.Emplace(CellDimensions - 2*CellDimensions/MembraneResolution*i,
+            CellDimensions);
+	}
+	for(int i = 0; i < MembraneResolution; i++)
+	{
+		Vertices2D.Emplace(-CellDimensions,
+            CellDimensions - 2*CellDimensions/MembraneResolution*i);
+	}
+
+	for(int i = 0; i < 50*CellDimensions; i++)
+    {
+        DrawMembrane();
+    }
+}
+    
+void UMembraneComponent::DrawMembrane(){
+
+    // Stores the temporary positions of the membrane.
+	TArray<FVector2D> NewPositions = Vertices2D;
+
+    // Loops through all the points in the membrane and relocates them as necessary.
+	for(size_t i = 0, end = NewPositions.Num(); i < end; i++)
+	{
+		const auto ClosestOrganelle = FindClosestOrganelles(Vertices2D[i]);
+        
+		if(!std::get<1>(ClosestOrganelle))
+		{
+			NewPositions[i] = (Vertices2D[(end+i-1)%end] + Vertices2D[(i+1)%end])/2;
+		}
+		else
+		{
+			const auto movementDirection = GetMovement(Vertices2D[i],
+                std::get<0>(ClosestOrganelle));
+            
+			NewPositions[i].X -= movementDirection.X;
+			NewPositions[i].Y -= movementDirection.Y;
+		}
+	}
+
+	// Allows for the addition and deletion of points in the membrane.
+	for(size_t i = 0; i < NewPositions.Num() - 1; i++)
+	{
+		// Check to see if the gap between two points in the membrane is too big.
+		if(FVector2D::Distance(NewPositions[i], NewPositions[(i+1)%NewPositions.Num()]) >
+            CellDimensions/MembraneResolution)
+		{
+			// Add an element after the ith term that is the average of the i and i+1 term.
+			const auto tempPoint = (NewPositions[(i + 1) % NewPositions.Num()] +
+                NewPositions[i])/2;
+            
+			NewPositions.Insert(tempPoint, i+1);
+
+			i++;
+		}
+
+		// Check to see if the gap between two points in the membrane is too small.
+		if(FVector2D::Distance(NewPositions[(i+1)%NewPositions.Num()], (NewPositions[(i-1) %
+                        NewPositions.Num()])) < CellDimensions/MembraneResolution)
+		{
+			// Delete the ith term.
+			NewPositions.RemoveAt(i);
+		}
+	}
+
+	Vertices2D = NewPositions;    
+}
+
+void UMembraneComponent::MakePrism(){
+
+    const double Height = .1;
+
+    MeshPoints.Empty();
+
+	for(size_t i = 0, end = Vertices2D.Num(); i < end; i++)
+	{
+		MeshPoints.Emplace(Vertices2D[i%end].X, Vertices2D[i%end].Y,
+            +Height/2);
+        
+		MeshPoints.Emplace(Vertices2D[(i+1)%end].X, Vertices2D[(i+1)%end].Y,
+            -Height/2);
+        
+		MeshPoints.Emplace(Vertices2D[i%end].X, Vertices2D[i%end].Y,
+            -Height/2);
+        
+		MeshPoints.Emplace(Vertices2D[i%end].X, Vertices2D[i%end].Y,
+            +Height/2);
+        
+		MeshPoints.Emplace(Vertices2D[(i+1)%end].X, Vertices2D[(i+1)%end].Y,
+            +Height/2);
+        
+		MeshPoints.Emplace(Vertices2D[(i+1)%end].X, Vertices2D[(i+1)%end].Y,
+            -Height/2);
+	}
+
+	for(size_t i = 0, end = Vertices2D.Num(); i < end; i++)
+	{
+		MeshPoints.Emplace(Vertices2D[i%end].X, Vertices2D[i%end].Y,
+            +Height/2);
+        
+		MeshPoints.Emplace(0,0,Height/2);
+        
+		MeshPoints.Emplace(Vertices2D[(i+1)%end].X, Vertices2D[(i+1)%end].Y,
+            +Height/2);
+
+		MeshPoints.Emplace(Vertices2D[i%end].X, Vertices2D[i%end].Y,
+            -Height/2);
+        
+		MeshPoints.Emplace(Vertices2D[(i+1)%end].X, Vertices2D[(i+1)%end].Y,
+            -Height/2);
+        
+		MeshPoints.Emplace(0,0,-Height/2);
+	}
+}
+
+void UMembraneComponent::CalcUVCircle(){
+
+    UVs.Empty();
+
+    for(size_t i = 0, end = MeshPoints.Num(); i < end; i++)
+    {
+        double x, y, z, a, b, c;
+        x = MeshPoints[i].X;
+        y = MeshPoints[i].Y;
+        z = MeshPoints[i].Z;
+
+        double ray = x*x + y*y + z*z;
+
+        double t = std::sqrt(ray)/(2.0*ray);
+        a = t*x;
+        b = t*y;
+        c = t*z;
+
+        UVs.Emplace(a+0.5,b+0.5// ,c+0.5
+        );
+    }
+}
+
+// ------------------------------------ //
+std::tuple<FVector2D, bool> UMembraneComponent::FindClosestOrganelles(FVector2D Target){
+    
+    // The distance we want the membrane to be from the organelles squared.
+	double closestSoFar = 4;
+	int closestIndex = -1;
+
+	for (size_t i = 0, end = OrganellePositions.Num(); i < end; i++)
+	{
+		double lenToObject =  FVector2D::DistSquared(Target, OrganellePositions[i]);
+
+		if(lenToObject < 4 && lenToObject < closestSoFar)
+		{
+			closestSoFar = lenToObject;
+
+			closestIndex = i;
+		}
+	}
+
+	if(closestIndex != -1)
+		return std::make_tuple(OrganellePositions[closestIndex], true);
+	else
+		return std::make_tuple(FVector2D(0, 0), false);
+    
+}
+
+FVector2D UMembraneComponent::GetMovement(FVector2D Target, FVector2D ClosestOrganelle){
+
+    double power = pow(2.7, FVector2D::Distance(-Target, ClosestOrganelle)/10)/50;
+
+	return (ClosestOrganelle - Target)*power;
+}
 
--- Source/Thrive/Microbe/MembraneComponent.h
+++ Source/Thrive/Microbe/MembraneComponent.h
@@ -7,6 +7,8 @@
 
 #include "RuntimeMeshComponent.h"
 
+#include <tuple>
+
 #include "MembraneComponent.generated.h"
 
 /**
@@ -29,9 +31,68 @@ public:
     //! Creates the mesh, needs to be called in actor OnConstruction
     void CreateMembraneMesh(URuntimeMeshComponent* GeometryReceiver);
 	
- protected:
+protected:
+
+    // Methods adapted from the old membrane system
+
+    //! \brief Renamed from the old Initialize method. Calculates the required size
+    //!
+    //! usually followed by calls to DrawMembrane and MakePrism.
+    //! Called automatically by CreateMembraneMesh
+    void DoMembraneIterativeShrink();
+    
+    //! Creates the 2D points in the membrane by looking at the positions of the organelles.
+	void DrawMembrane();
+
+    //! Creates a 3D prism from the 2D vertices.
+	void MakePrism();
+
+    //! Finds the UV coordinates be projecting onto a plane and stretching to fit a circle.
+	void CalcUVCircle();
+
+
+    // Helpers for the current "iterative shrink" method of making the
+    // membrane fit the organelles
+
+    //! Return the position of the closest organelle to the target
+    //! point if it is less then a certain threshold away.
+    //! \returns Second element is false if nothing was found
+    std::tuple<FVector2D, bool> FindClosestOrganelles(FVector2D Target);
+
+	//! Decides where the point needs to move based on the position of the closest organelle.
+	FVector2D GetMovement(FVector2D Target, FVector2D ClosestOrganelle);
+
+protected:
 
     UPROPERTY(BlueprintReadOnly)
     UOrganelleContainerComponent* OrganelleContainerComponent = nullptr;
+
+    //! The length in pixels of a side of the square that bounds the membrane.
+    //! Half the side length of the original square that is compressed to make the membrane.
+    UPROPERTY(BlueprintReadOnly)
+    int32 CellDimensions = 10;
+
+    //! The amount of points on the side of the membrane.
+    //! Amount of segments on one side of the above described square.
+    UPROPERTY(BlueprintReadOnly)
+	int32 MembraneResolution = 10;
+
     
+
+    // The mesh data that is written when generating and then used to
+    // update the RuntimeMeshComponent
+
+    //! Stores the Mesh in a vector such that every 3 points make up a triangle.
+    TArray<FVector> MeshPoints;
+
+    //! Stores the UV coordinates for the MeshPoints.
+    TArray<FVector2D> UVs;
+
+    //! These are initially placed far away and shrink towards the organelles
+    TArray<FVector2D> Vertices2D;
+
+    // Temporary data for generation //
+
+    //! Stores organelle data during generation
+    TArray<FVector2D> OrganellePositions;
 };
