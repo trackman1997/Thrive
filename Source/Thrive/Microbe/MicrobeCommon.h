14e6e22b08c76b015cb743fd6bed6600862c6ada
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2017-06-10 23:06:08 +0300

    Made the compound id work with blueprints

--- Source/Thrive/Microbe/MicrobeCommon.h
+++ Source/Thrive/Microbe/MicrobeCommon.h
@@ -3,13 +3,14 @@
 
 #pragma once
 
-#include <cstdint>
+#include "CoreMinimal.h"
 
 //! Compound type ids
 //!
 //! This is an enum to prevent implicit convertions from and to integers
 //! These are dynamically assigned by the UCompoundRegistry object
-enum class ECompoundID : int32_t{
+UENUM()
+enum class ECompoundID : int32{
 
     Invalid = -1
 };
